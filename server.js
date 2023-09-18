454cddf05e8da3c20a01bcd922208644874f812c
deiu25 <deiu25@gmail.com>
2023-09-18 09:50:16 +0300

    initial commit

---
+++ server.js
@@ -0,0 +1,54 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const userRoute = require("./routes/userRoute");
+const errorHandler = require("./middleware/errorMiddleware");
+
+const app = express();
+
+// Funcția middleware pentru setarea header-urilor
+function setCOOPHeaders(req, res, next) {
+  res.setHeader("Cross-Origin-Opener-Policy", "same-origin-allow-popups");
+    res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
+    next();
+}
+
+// Utilizează middleware-ul înainte de orice alt middleware/rută
+app.use(setCOOPHeaders);
+
+// Alte middleware-uri
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "https://authz-app.vercel.app"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+
+// Routes
+app.use("/api/users", userRoute);
+
+app.get("/", (req, res) => {
+  res.send("Home Page");
+});
+
+// Error Handler Middleware
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((err) => console.log(err));
