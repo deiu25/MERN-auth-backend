fb856c0665c8e29847d1e883c1e2ea199741d1f7
deiu25 <deiu25@gmail.com>
2023-09-18 10:41:32 +0300

    frontend url

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://authz-app.vercel.app"],
+    origin: ["http://localhost:3000", "https://syntaxseeker-auth.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
